b188d97fd21439f78578386c022e15cd61c8940c
xtremelyfruity <adavyasharma@gmail.com>
2025-05-06 19:19:50 -0700

    Static Final

--- next.config.js
+++ next.config.js
@@ -1,9 +1,23 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["v0.blob.com"],
-  },
-}
 
-module.exports = nextConfig
+  // Tell Next to emit a static-exportable site:
+  output: 'export',
+  // Optional but recommended for clean URLs on GitHub Pages:
+  trailingSlash: true,
+
+  images: {
+    domains: ['v0.blob.com'], // your external image host
+  },
+
+  // If you're deploying to https://adavyas.github.io (a USER site),
+  // you _do not_ need a basePath. If this were a project page
+  // (username.github.io/repo-name), you'd add:
+  // basePath: '/repo-name',
+  // assetPrefix: '/repo-name/',
+};
+
+module.exports = nextConfig;
